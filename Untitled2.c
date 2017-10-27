7faa63b9944177590975398762ce950a560242d5
konika4 <32925914+konika4@users.noreply.github.com>
2017-10-27 09:35:52 +0600

    Add files via upload
    array file 
    a2 doc

---
+++ Untitled2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+{
+    int i, n;
+    printf("Print all even numbers till: ");
+    scanf("%d", &n);
+
+    printf("All even numbers from 1 to 20 are: \n", n);
+    for(i=2; i<=n; i+=2)
+    {
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
+}
+
